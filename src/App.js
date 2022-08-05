bb1092eacf97969474023b4994beab7aa0d0de37
nicolas_schmidt <nicolasschmidt@deltaglobal.com.br>
2022-08-05 18:40:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import People from './Assets/People.svg';
+import Arrow from './Assets/arrow_button.svg';
+import Trash from './Assets/icon_delete.svg';
+import { 
+  Container, 
+  H1, 
+  Image, 
+  ContainerItens, 
+  InputLabel, 
+  Input, 
+  Button,
+  User
+} from './styles';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef();
+  const inputAge = useRef();
+
+  function addNewUser() {
+    setUsers([...users,{id: Math.random(), name:inputName.current.value , age:inputAge.current.value}])
+  }
+
+  function deleteUser(userId) {
+    const newUsers = users.filter(user => user.id !== userId)
+    setUsers(newUsers);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Image alt="logo-image" src={People}/>
+      <ContainerItens>
+        <H1>Olá!</H1>
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder="Nome"/>
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder="Idade"/>
+
+        <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Arrow}/></Button>
+
+        <ul>
+          { users.map( user => (
+            <User key={user.id}>
+              <p>{user.name}</p><p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img alt="Delete Icon" src={Trash}/>
+              </button>
+            </User>
+          ))
+          }
+          
+        </ul>
+      </ContainerItens>
+      
+    </Container>
+  )
 }
 
 export default App;
